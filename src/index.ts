ea0f874f8c510a01fa6e2a647ed1f21a34f9b239
skydog221 <skydog221@outlook.com>
2025-10-04 21:10:03 +0800

    feat(getI10n):新增I10n便捷获取函数

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,68 @@
-(function (Scratch) {
+import i10n from './l10n'
+;(function (Scratch) {
   if (Scratch.extensions.unsandboxed === false) {
-    throw new Error("Sandboxed mode is not supported");
+    throw new Error('Sandboxed mode is not supported')
   }
   // Your extension's code
   class YourExtension implements Scratch.Extension {
-    runtime: VM.Runtime;
+    runtime: VM.Runtime
     constructor(runtime: VM.Runtime) {
-      this.runtime = runtime;
+      this.runtime = runtime
+    }
+    getI10n(id: string) {
+      return Scratch.translate({ id: id, default: id })
     }
     getInfo() {
       return {
-        id: "newExtension",
+        id: 'newExtension',
         name: "FurryR's example extension",
         blocks: [
           {
             blockType: Scratch.BlockType.COMMAND,
-            opcode: "test",
-            text: "1",
-          },
-        ],
-      };
+            opcode: 'test',
+            text: '1'
+          }
+        ]
+      }
     }
     test(args: Record<string, string>, util: VM.BlockUtility) {
-      console.log("Hello World", args, util);
+      console.log('Hello World', args, util)
     }
   }
 
   if (!Scratch.vm.runtime.gandi) {
     // For Turbowarp
-    Scratch.extensions.register(new YourExtension(Scratch.runtime));
+    Scratch.extensions.register(new YourExtension(Scratch.runtime))
   } else {
     // For Gandi
     window.tempExt = {
       Extension: YourExtension,
       info: {
-        extensionId: "confetti",
-        name: "confetti.name",
-        description: "confetti.description",
-        iconURL: "",
+        extensionId: 'confetti',
+        name: 'confetti.name',
+        description: 'confetti.description',
+        iconURL: '',
         featured: false,
         disabled: false,
         collaboratorList: [
           {
-            collaborator: "多bug的啸天犬 @ CCW",
-            collaboratorURL: "https://ccw.site/student/197354885",
-          },
-        ],
+            collaborator: '多bug的啸天犬 @ CCW',
+            collaboratorURL: 'https://ccw.site/student/197354885'
+          }
+        ]
       },
       l10n: {
-        "zh-cn": {
-          "confetti.name": "彩带喷射",
-          "confetti.description":
-            "在 Scratch 中喷射彩带！不受舞台限制，在整个网页上喷射吧~",
+        'zh-cn': {
+          'confetti.name': '彩带喷射',
+          'confetti.description':
+            '在 Scratch 中喷射彩带！不受舞台限制，在整个网页上喷射吧~'
         },
         en: {
-          "confetti.name": "Confetti",
-          "confetti.description":
-            "Spray confetti in Scratch! Unrestricted by the stage, spray all over the web~",
-        },
-      },
-    };
+          'confetti.name': 'Confetti',
+          'confetti.description':
+            'Spray confetti in Scratch! Unrestricted by the stage, spray all over the web~'
+        }
+      }
+    }
   }
-})(Scratch);
+})(Scratch)
